f4885cb32dd53fd160b00f6c4d99851bf315efc7
smyhw <i@smyhw.online>
2020-09-13 11:33:27 +0800

    增加参数
    增加参数，默认允许仅回溯单条消息

--- online.smyhw.KQ.chat_backup.Code/Event_GroupMessage.cs
+++ online.smyhw.KQ.chat_backup.Code/Event_GroupMessage.cs
@@ -1,4 +1,4 @@
-﻿using Native.Sdk.Cqp.EventArgs;
+using Native.Sdk.Cqp.EventArgs;
 using Native.Sdk.Cqp.Interface;
 using Native.Sdk.Cqp.Model;
 using System;
@@ -28,19 +28,38 @@ namespace online.smyhw.KQ.chat_backup.Code
             if (e.Message.Text.StartsWith("//chat_backup") || e.Message.Text.StartsWith("//cbp")) 
             {
                 string[] temp1 = e.Message.Text.Split(' ');
-                if (temp1.Length != 2) 
+                if (temp1.Length < 2) 
                 {
-                    e.FromGroup.SendGroupMessage("格式错误，正确的格式为：//chat_backup <消息条数> 或 //cbp <消息条数>");
+                    e.FromGroup.SendGroupMessage("格式错误!");
+                    sendHelp(e.FromGroup);
                     return;
                 }
                 int num;
+                Boolean ListMode = false;
+                Boolean NameMode = true;
+                for (int i = 2; i < temp1.Length; i++)
+                {
+                    switch (temp1[i])
+                    {
+                        case "-l":
+                            ListMode = true;
+                            break;
+                        case "-n":
+                            NameMode = false;
+                            break;
+                        default:
+                            e.FromGroup.SendGroupMessage("未知参数<"+temp1[i]+">");
+                            sendHelp(e.FromGroup);
+                            return;
+                    }
+                }
                 try
                 {
                     num=int.Parse(temp1[1]);
                 }
                 catch (Exception ee)
                 {
-                    e.FromGroup.SendGroupMessage("<消息条数> 不是数字！");
+                    e.FromGroup.SendGroupMessage("<"+temp1[1]+">无法被识别为数字！");
                     return;
                 }
 
@@ -56,24 +75,98 @@ namespace online.smyhw.KQ.chat_backup.Code
                     e.FromGroup.SendGroupMessage("没有足够相关消息缓存{"+ temp2.Count+" < "+num+"}");
                     return;
                 }
-                e.FromGroup.SendGroupMessage("消息回溯<"+num+">条");
-                for (; num > 0; num--)
-                {
-                    e.FromGroup.SendGroupMessage(temp2[temp2.Count-num]);
+                if (ListMode)
+                {//输出完整的消息列表 
+                    e.FromGroup.SendGroupMessage("消息回溯<" + num + ">条");
+                    for (; num > 0; num--)
+                    {
+                        String fin = "";
+                        if (NameMode)
+                        {
+                            cbp_message tmp1 = (cbp_message)temp2[temp2.Count - num];
+                            fin = tmp1.getFromName() + ":" + tmp1.getMsg();
+                        }
+                        else
+                        {
+                            cbp_message tmp1 = (cbp_message)temp2[temp2.Count - num];
+                            fin = tmp1.getFromQQ() + ":" + tmp1.getMsg();
+                        }
+                        e.FromGroup.SendGroupMessage(fin);
+                    }
                 }
+                else
+                {
+                    String fin = "";
+                    if (NameMode)
+                    {
+                        cbp_message tmp1 = (cbp_message)temp2[temp2.Count - num];
+                        fin = tmp1.getFromName() + ":" + tmp1.getMsg();
+                    }
+                    else
+                    {
+                        cbp_message tmp1 = (cbp_message)temp2[temp2.Count - num];
+                        fin = tmp1.getFromQQ() + ":" + tmp1.getMsg();
+                    }
+                    e.FromGroup.SendGroupMessage("消息回溯第<" + num + ">条");
+                    e.FromGroup.SendGroupMessage(fin);
+                }
+
 
             }
             else
-            {
+            {//缓存消息
                 if (!GroupList.ContainsKey(e.FromGroup.Id))
                 { GroupList.Add(e.FromGroup.Id, new ArrayList()); }
                 ArrayList temp1 = (ArrayList)GroupList[e.FromGroup.Id];
-                temp1.Add(e.FromQQ.Id + ":" + e.Message.Text);
+                temp1.Add(new cbp_message(e.FromQQ.Id, e.FromGroup, e.Message.Text));
                 if (temp1.Count > 15) { temp1.RemoveAt(0); }
                 GroupList[e.FromGroup.Id] = temp1;
             }
             // 设置该属性, 表示阻塞本条消息, 该属性会在方法结束后传递给酷Q
 //            e.Handler = true;
+        }
+
+        static void sendHelp(Group tar)
+        {
+            tar.SendGroupMessage("用法://chat_backup <消息条数> [参数]\n" +
+                "或 //cbp <消息条数> [参数]\n" +
+                "-l   #查询到目标条数的完整列表\n" +
+                "-n   #使用QQ号代替群昵称\n");
+        }
+    }
+
+    /**
+     * 被设计来存储一条消息
+     */
+    class cbp_message 
+    {
+        long FromQQ;
+        Group FromGroup;
+        String FromName;
+        String msg;
+        public cbp_message(long FromQQ, Group FromGroup,String msg)
+        {
+            this.FromQQ = FromQQ;
+            this.msg = msg;
+            GroupMemberInfo tmp1 = FromGroup.GetGroupMemberInfo(FromQQ, true);
+            FromName = tmp1.Card;
+        }
+
+        public long getFromQQ()
+        {
+            return this.FromQQ;
+        }
+        public Group getFromGroup()
+        {
+            return this.FromGroup;
+        }
+        public String getFromName()
+        {
+            return this.FromName;
+        }
+        public String getMsg()
+        {
+            return this.msg;
         }
     }
 }
